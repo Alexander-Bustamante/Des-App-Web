a149dcc075ad71e779c666b08a77ff053966d18c
Alexander-Bustamante <al.bustamante.cl@gmail.com>
2025-07-07 11:14:32 -0400

    ok

---
+++ 21_metodos_push_/script.js
@@ -0,0 +1,10 @@
+console.log("Funcionando correctamente...")
+
+// Ejercicio 1: Agregar elementos //
+
+function agregar_elementos(){
+    compras = []
+    compras.push("pan", "leche", "Huevos")
+
+    alert(`Las compras son: ${compras.join(" - ")}`)
+}
