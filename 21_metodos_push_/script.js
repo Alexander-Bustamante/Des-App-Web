73e71cafa09d48e0c455739dd8a23e73d48df048
Alexander-Bustamante <al.bustamante.cl@gmail.com>
2025-07-08 11:19:04 -0400

    ok

--- 21_metodos_push_/script.js
+++ 21_metodos_push_/script.js
@@ -1,10 +1,40 @@
 console.log("Funcionando correctamente...")
 
+const resultado = document.getElementById("resultado");
+let colores = ["rojo", "azul"];
+
 // Ejercicio 1: Agregar elementos //
 
 function agregar_elementos(){
-    compras = []
-    compras.push("pan", "leche", "Huevos")
+    let elemento = document.getElementById("lista").value;
 
-    alert(`Las compras son: ${compras.join(" - ")}`)
+    // Crear elemento HTML --> createElement //
+    let item = document.createElement("li");
+
+    item.innerText = elemento; // <li>--InnerText--</li> //
+
+    // appendChild inserta valor hijo en padre
+    // resultado(<ul>(item(<li></li>))</ul>)
+    resultado.appendChild(item);
+}
+
+// Ejercicio 2: Agregar elementos //
+
+function quitar_el_ultimo(){
+    let elemento = document.getElementById("lista").value   
+
+    let item = document.removeChild("li")
+
+    item.innerText = elemento
+
+    resultado.removeChild("li")
+}
+
+// Ejercicio 2: Agregar elementos //
+
+function agregar_al_inicio(){
+
+    colores.unshift("amarillo");
+    
+    alert(`Las compras son: ${colores.join(" - ")}`);
 }
